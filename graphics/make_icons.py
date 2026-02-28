f11589c1e4bebd5c2913461e11151538e422e292
Pier-Yves Lessard <py.lessard@gmail.com>
2026-02-27 20:06:18 -0500

    handle transparency

--- graphics/make_icons.py
+++ graphics/make_icons.py
@@ -4,11 +4,37 @@ from PIL import Image
 import os
 import json
 
-from typing import Dict, TypedDict, List, cast  
+from typing import Dict, TypedDict, List, cast
+
 
 class Format(TypedDict):
-    src:str
-    formats:List[List[int]]
+    src: str
+    formats: List[List[int]]
+
+
+def premultiply(im):
+    pixels = im.load()
+    for y in range(im.size[1]):
+        for x in range(im.size[0]):
+            r, g, b, a = pixels[x, y]
+            if a != 255:
+                r = r * a // 255
+                g = g * a // 255
+                b = b * a // 255
+                pixels[x, y] = (r, g, b, a)
+
+
+def unmultiply(im):
+    pixels = im.load()
+    for y in range(im.size[1]):
+        for x in range(im.size[0]):
+            r, g, b, a = pixels[x, y]
+            if a != 255 and a != 0:
+                r = 255 if r >= a else 255 * r // a
+                g = 255 if g >= a else 255 * g // a
+                b = 255 if b >= a else 255 * b // a
+                pixels[x, y] = (r, g, b, a)
+
 
 def main():
     parser = argparse.ArgumentParser()
@@ -17,7 +43,7 @@ def main():
     args = parser.parse_args()
 
     os.chdir(os.path.join(os.path.dirname(__file__)))
-    
+
     common_file = 'common.json'
     theme_file = args.theme + '.json'
     with open(common_file) as f:
@@ -33,13 +59,16 @@ def main():
 
     for format_dict in [common_formats, theme_formats]:
         for name, config in format_dict.items():
-            src_image = Image.open( config['src'] )
+            src_image = Image.open(config['src'])
             for format in config['formats']:
                 assert len(format) == 2
 
-                #dst = src_image.copy()
-                dst = src_image.resize(tuple(format), Image.Resampling.LANCZOS)
+                dst = src_image.convert(mode='RGBA')
+                premultiply(dst)
+                dst = dst.resize(tuple(format), Image.Resampling.LANCZOS)
+                unmultiply(dst)
                 dst.save(os.path.join(args.output, f"{name}_{format[0]}x{format[1]}.png"), "PNG")
+
 
 if __name__ == '__main__':
     sys.exit(main())
